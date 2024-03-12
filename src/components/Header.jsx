84160a7666667e0a966fcca0b89bbde157b9e8ae
Gabriel Medeiros <107512629+Gabriel-Med@users.noreply.github.com>
2024-03-11 21:36:34 -0300

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import { NavLink } from "react-router-dom";
+import {auth} from '../firebase/config.js';
+import { signOut } from "firebase/auth";
+import {useDispatch} from 'react-redux';
+import {setUser} from '../store/usersSlice.js';
+
+function Header({pageTitle}) {
+
+  const dispatch = useDispatch();
+
+  function handleSingOut(){
+    
+    if(confirm('Deseja realmente sair?')) {
+      signOut(auth).then(() => {
+        dispatch(setUser(null));
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+  }
+
+    return (
+      <>
+
+            <h1>{pageTitle}</h1>
+
+            <div className="header-btns">
+
+                    <NavLink to="/">
+                      <button className="btn">
+                          Books
+                      </button>
+                    </NavLink>
+
+                    <NavLink to="/add-book">
+                      <button className="btn">
+                          Add Book +
+                      </button>
+                    </NavLink>
+
+                    <button onClick = {handleSingOut} className="btn transparent">
+                      Logout
+                    </button>
+
+               
+            </div>
+    
+      </>
+    )
+  }
+  
+  export default Header
+  
---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAlk7c42MMsEAYcS7bx9-qmPl1KBS0mnQE",
+  authDomain: "lista-de-livros-com-firebase.firebaseapp.com",
+  projectId: "lista-de-livros-com-firebase",
+  storageBucket: "lista-de-livros-com-firebase.appspot.com",
+  messagingSenderId: "70429373020",
+  appId: "1:70429373020:web:353d541e784cd96e14ec99"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
