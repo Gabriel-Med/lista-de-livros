36c0b75f5a60da626d0af31f69e4c833c8011382
Gabriel Medeiros <107512629+Gabriel-Med@users.noreply.github.com>
2024-03-25 12:19:07 -0300

    setup github workflow

--- src/views/BooksPage.jsx
+++ src/views/BooksPage.jsx
@@ -1,28 +1,47 @@
 import Book from '../components/Book.jsx';
 import Header from '../components/Header.jsx';
-import {useSelector} from 'react-redux';
-import {selectBooks} from '../store/booksSlice.js';
+import {useSelector, useDispatch} from 'react-redux';
+import {selectBooks, fetchBooks} from '../store/booksSlice.js';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function BooksPage() {
 
-  const books = useSelector(selectBooks);
+  const dispatch = useDispatch();
+  const books = useSelector(selectBooks).books;
   const pageTitle = "📖 Book List with Router, Redux & Firebase";
-    
-    
+
+  const bookStatus = useSelector(selectBooks).status;
+
+  useEffect(() => {
+    if(bookStatus == 'idle'){
+      dispatch(fetchBooks());
+    }
+
+  }, [])
+
+
     return (
       <>
         <div className="container">
             <Header pageTitle={pageTitle} />
             <div className="books-container">
-                <div className="books-list">
-                    
-                    {books.map(book => 
-                    
-                    <Book key={book.id} book={book}  />
-                    
-                    )}
 
-                </div>
+              { books.length ?
+                
+                  <div className="books-list">
+                      
+                      {books.map(book => 
+                      
+                      <Book key={book.id} book={book}  />
+                      
+                      )}
+
+                  </div> : bookStatus == 'loading' ? 
+                  <div>Loading... </div> : 
+                  <div> Your book list is empty. <Link to = '/add-book'>Click here</Link> to add a new book. </div>
+
+              }
             </div>
         </div>
       </>
--- src/views/SingleBookPage.jsx
+++ src/views/SingleBookPage.jsx
@@ -1,27 +1,58 @@
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import Notes from '../components/Notes.jsx';
-import {useSelector, useDispatch} from 'react-redux';
-import {selectBooks, eraseBook, toggleRead} from '../store/booksSlice.js';
-import {eraseBookNotes} from '../store/notesSlice.js';
+import {useState, useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import {eraseBook, toggleRead} from '../store/booksSlice.js';
+import {doc, getDoc} from "firebase/firestore";
+import {db} from '../firebase/config.js';
 
 function SingleBookPage() {
   
   const dispatch = useDispatch();
   const navigate = useNavigate();
+ 
+
+  const {id} = useParams();
+  const [book, setBook] = useState('')
+  const [fetchStatus, setFetchStatus] = useState('idle')
+
 
   function handleEraseBook(id) {
     if(confirm('Are you sure you want to erase this book and all notes associated with it?')){
       dispatch(eraseBook(id));
-      dispatch(eraseBookNotes(id));
       navigate("/");
     }
   }
 
-  const {id} = useParams();
+  function handleToggleRead(info) {
+    dispatch(toggleRead({id: info.id, isRead: info.isRead}))
+    setBook({...book, isRead: !info.isRead});
+  }
 
-  const books = useSelector(selectBooks);
+  const fetchBooks = async(book_id) => {
 
-  const book = books.filter(book => book.id == id)[0];
+    try {
+      
+      const docRef = doc(db, "books", book_id);
+      const docSnap = await getDoc(docRef);
+  
+      if (docSnap.exists()) {
+        setBook({...docSnap.data(), id: docSnap.id})
+      } 
+      setFetchStatus('success')
+    } catch(err){
+      console.log('error', err);
+      setFetchStatus('error')
+    }
+  }
+
+  useEffect(() => {
+    if(fetchStatus  == 'idle'){
+      fetchBooks(id);
+    }
+
+  }, [])
+    
     
     return (
       <>
@@ -46,7 +77,7 @@ function SingleBookPage() {
                     <p>{book.synopsis}</p>
                     <div className="read-checkbox">
                         <input 
-                          onClick={()=>{dispatch(toggleRead(book.id))}}
+                          onClick={()=>{handleToggleRead({id: book.id, isRead: book.isRead})}}
                           type="checkbox" 
                           defaultChecked={book.isRead} />
                         <label>{ book.isRead ? "Already Read It" : "Haven't Read it yet" }</label>
@@ -60,10 +91,22 @@ function SingleBookPage() {
               <Notes bookId={id} />
             </div> 
             
-            : 
+            : fetchStatus == 'success' ? 
             
             <div>
               <p>Book not found. Click the button above to go back to the list of books.</p>
+            </div>
+            
+            : fetchStatus == 'error' ? 
+            
+            <div>
+              <p>Error fetching the book</p>
+            </div>
+            
+            : 
+
+            <div>
+              <p>Loading...</p>
             </div>
 
             }
